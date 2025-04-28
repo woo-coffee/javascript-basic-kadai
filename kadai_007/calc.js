4fa5694dd30bf1827d63bdac25dca80929e3760b
woo-coffee <116089404+woo-coffee@users.noreply.github.com>
2025-04-29 07:49:44 +0900

    kadai_007

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -3,20 +3,18 @@ let num = 15
 //numの値を出力する
 console.log(num);
 
-
+//変数numが3と5の倍数の場合： “3と5の倍数です”
+if(num % 3 == 0 && num % 5 == 0){
+    console.log("3と5の倍数です")
+}
 
 //変数numが3の倍数の場合： “3の倍数です"
-if (num % 3 == 0) {
+else if (num % 3 == 0) {
     console.log("３の倍数です")
 }
 //変数numが5の倍数の場合： “5の倍数です”
 else if(num % 5 == 0) {
     console.log("５の倍数です")
-}
-
-//変数numが3と5の倍数の場合： “3と5の倍数です”
-if(num % 3 == 0 && num % 5 == 0){
-    console.log("3と5の倍数です")
 }
 
 //それ以外の場合： 変数numの値を出力する
