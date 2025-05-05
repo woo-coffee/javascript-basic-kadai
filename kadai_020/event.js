7fe45663ee32bd29b85fea6a937e5cb7c808a8e8
woo-coffee <116089404+woo-coffee@users.noreply.github.com>
2025-05-06 08:49:54 +0900

    kadai020

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,7 +1,10 @@
 //画面から取得したHTML要素が定数に代入されている
+const text = document.getElementById('text')
 const btn = document.getElementById('btn')
+
 
 //ボタンをクリックした際に正しく文字変更がされている
 btn.addEventListener('click',() => {
-    console.log('ボタンをクリックしました');
+    const text = document.createElement('h2');
+    text.textContent('ボタンをクリックしました');
 });
